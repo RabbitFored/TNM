670903c92fc267803ac1b3c5a5ca13df0ea26c5f
RabbitFoRed <73241991+RabbitFored@users.noreply.github.com>
2024-06-22 13:23:37 +0000

    UPDATE

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-
-# importing packages
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 from rl import rl
 
-# reading the database
+yrs = ["2021", "2022", "2023"]
+
 
 def show_rl_plot(yr):
     data = pd.read_json(f"data/TNMC-PR-LIST/{yr}/TNMC-PR-LIST-{yr}.json")
@@ -13,13 +12,15 @@ def show_rl_plot(yr):
     plt.title(f"TN RANKLIST - {yr}")
     plt.show()
 
-    
+
 def gen_data(yr):
-    rl(map=f"assets/docs/rl-maps/TNMC-PR-LIST-{yr}.map.json" , rlist=f"assets/docs/TNMC-PR-LIST-{yr}.pdf", file_name=f"data/TNMC-PR-LIST/{yr}/TNMC-PR-LIST-{yr}.json")
-    
+    rl(map=f"assets/docs/rl-maps/TNMC-PR-LIST-{yr}.map.json",
+       rlist=f"assets/docs/TNMC-PR-LIST-{yr}.pdf",
+       file_name=f"data/TNMC-PR-LIST/{yr}/TNMC-PR-LIST-{yr}.json")
+
+
 def gen_show_rl_plot(yr):
     gen_data(yr)
     show_rl_plot(yr)
-
-yr = "2023"
-gen_show_rl_plot(yr)
+    
+show_rl_plot(yrs[2])
