54f87f5681763c490be51e002e23927900cc4d1d
RabbitFoRed <73241991+RabbitFored@users.noreply.github.com>
2024-06-22 13:28:14 +0000

    Update

--- main.py
+++ main.py
@@ -1,26 +1,3 @@
-import seaborn as sns
-import matplotlib.pyplot as plt
-import pandas as pd
-from rl import rl
+from rl_plot import yrs, show_plot
 
-yrs = ["2021", "2022", "2023"]
-
-
-def show_rl_plot(yr):
-    data = pd.read_json(f"data/TNMC-PR-LIST/{yr}/TNMC-PR-LIST-{yr}.json")
-    sns.histplot(x='mark', data=data, kde=True, hue='comm')
-    plt.title(f"TN RANKLIST - {yr}")
-    plt.show()
-
-
-def gen_data(yr):
-    rl(map=f"assets/docs/rl-maps/TNMC-PR-LIST-{yr}.map.json",
-       rlist=f"assets/docs/TNMC-PR-LIST-{yr}.pdf",
-       file_name=f"data/TNMC-PR-LIST/{yr}/TNMC-PR-LIST-{yr}.json")
-
-
-def gen_show_rl_plot(yr):
-    gen_data(yr)
-    show_rl_plot(yr)
-    
-show_rl_plot(yrs[2])
+show_plot(yrs[1])
